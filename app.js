bcee28639791b8d58d9d1458cd473c99eb5b9b57
Priyank Aswal <aswalpriyank0@gmail.com>
2025-01-12 14:39:11 +0530

    modified app.js for binding server

--- app.js
+++ app.js
@@ -90,7 +90,7 @@ app.use(errorController.get404);
 const PORT = process.env.PORT || 3000;
 
 mongoose.connect(MONGO_DB_URL).then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server running at http://localhost:${PORT}`);
+  app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
   });
 });
